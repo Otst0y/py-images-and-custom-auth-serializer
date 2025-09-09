8b5d68f2bb00ddd2ab5289da0c34a377e0dda63f
Otstoy <otstoy.sxe@gmail.com>
2025-09-09 12:38:42 +0300

    chore: implement feature to add image for movie; replace username field with email field in User model.

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework import routers, views
 
 from cinema.views import (
     GenreViewSet,
@@ -18,6 +18,8 @@ router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
 router.register("orders", OrderViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+]
 
 app_name = "cinema"
